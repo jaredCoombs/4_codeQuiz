10a038581f4badc588e0b24872b1fb9e76e5cdce
Jared Coombs <jared.coombs@gmail.com>
2020-02-12 21:53:53 -0800

    added wireframe

--- qiuiz.js
+++ qiuiz.js
@@ -1,1 +1,20 @@
-console.log = ("hello!");
+
+// container for start quiz
+    // begin with a start button
+
+//quiz in a container
+  //questions
+  // five questions 
+    //choices
+    //incorrect answer = time penalty
+  //timer
+    // constant countdown
+    // speeds up with incorrect answers
+
+//score container
+//clock visible at all times
+  //counter reaches 0 game over
+  // game over add initials
+  // top 10?
+
+
