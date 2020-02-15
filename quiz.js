4b2a37495d6aced1ab67f6cf032c3d4dc6f80b73
Jared Coombs <jared.coombs@gmail.com>
2020-02-15 02:37:30 -0800

    still having difficulty with function of the quiz. built the timer in another window. It worked fine. still not working in app. button will still not start timer or print questions. not sure why.

--- quiz.js
+++ quiz.js
@@ -1,4 +1,14 @@
 
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var counter = document.getElementById("counter");
+var timeGauge = document.getElementById("timeGauge");
+var scoreDiv = document.getElementById("scoreContainer");
+
 var button = document.getElementById("button");
 var title = document.getElementById("timer");
 var time = 60;
@@ -9,5 +19,56 @@ button.addEventListener("click", function (event) {
     time--;
     title.innerHTML = time;
   }, 1000);
+
+
+  let questions = [
+    // five questions
+    //choices
+    {
+      question: "What does HTML stand for?",
+      choiceA: "Hyperactive Test Mode Length",
+      choiceB: "Hyper Tetxt Markup Language",
+      choiceC: "High Timed Math Logic",
+      correct: "A",
+
+
+      question: "Is HTML a programming language?",
+      choiceA: "Hyperactive Test Mode Length",
+      choiceB: "anser",
+      choiceC: "answer",
+      correct: "A",
+
+
+      question: "What do you use to make a plain webpage look pretty?",
+      choiceA: "Hyperactive Test Mode Length",
+      choiceB: "anser",
+      choiceC: "answer",
+      correct: "A",
+
+
+      question: "What does HTML stand for?",
+      choiceA: "Hyperactive Test Mode Length",
+      choiceB: "anser",
+      choiceC: "answer",
+      correct: "A",
+
+      question: "What does HTML stand for?",
+      choiceA: "Hyperactive Test Mode Length",
+      choiceB: "anser",
+      choiceC: "answer",
+      correct: "A",
+
+    }
+
+
+
+  ]
+
+
+
+
+
+
+
 
 });
